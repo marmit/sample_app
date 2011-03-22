16d4f3f190c49d24f219640fa04260e0834604cc
John Lassiter <mecrobmarmit@yahoo.com>
2011-03-22 15:55:05 -0600

    Added links with routes

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -36,7 +36,7 @@ describe PagesController do
 	end
   end
 
-    describe "GET 'about'" do
+   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
@@ -45,6 +45,18 @@ describe PagesController do
     	get 'about'
     	response.should have_selector("title", 
     	                              :content => "#{@base_title} | About")
+	end
+  end
+  
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+    it "should have the right title" do
+    	get 'help'
+    	response.should have_selector("title", 
+    	                              :content => "#{@base_title} | Help")
 	end
   end
   
